d256543cea50139b87ce6633250967aeb7940e80
uuronn <uuronprogram@gmail.com>
2022-01-05 23:33:47 +0900

    ずっとエラーなりやがる

--- components/template/timer.vue
+++ components/template/timer.vue
@@ -1,44 +1,31 @@
 <template>
   <div>
-    {{ countDown }}
+    <div>
+      {{ countDown }}
+    </div>
+    <button @click="countDownTimer">ボタン</button>
   </div>
 </template>
 
 <script>
 export default {
-  created() {
-    window.setInterval(autoIncrementTimer, 1000);
-  },
-  methods: {
-    autoIncrementTimer() {
-      if (this.timerCount <= 0) {
-      }
-      timerCount -= 1;
-    }
-  },
   data() {
     return {
-      timerCount: 100
+      countDown: 10
     };
+  },
+  methods: {
+    countDownTimer() {
+      if (this.countDown > 0) {
+        setTimeout(() => {
+          this.countDown -= 1;
+          this.countDownTimer();
+        }, 1000);
+      }
+    }
+  },
+  created() {
+    this.countDownTimer();
   }
 };
 </script>
-
-<style scoped lang="scss">
-.timer {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &__show {
-    font-size: 100px;
-  }
-
-  &__button {
-    border-radius: 20px;
-    width: 80px;
-    height: 30px;
-    background: red;
-  }
-}
-</style>
